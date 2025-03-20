16549ec5a8ba216d809f223945b003d218d57e4b
Paul Sander <pasander@ethz.ch>
2025-03-20 15:20:54 +0100

    linked second image

--- plots.py
+++ plots.py
@@ -1,6 +1,10 @@
 from main import layout
 import matplotlib.pyplot as plt
 
-# First image only showing the axis
+# First image: only showing the axis
 fig, ax = layout(light=False)
 plt.savefig("./img/empty.svg")
+
+# Second image: introduction of light
+fig, ax = layout()
+plt.savefig("./img/light.svg")
